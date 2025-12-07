411c115e4aadf4c1f24a6e08ea75f8215ce02004
vkir26 <vanya.kireew2012@yandex.ru>
2025-12-07 16:21:45 +0300

    Implementation of a parser and relational database

--- multi_baker.py
+++ multi_baker.py
@@ -4,20 +4,49 @@ from pathlib import Path
 from dataclasses import dataclass
 
 
-def read_csv(filepath: Path) -> list[dict[str, str]]:
+@dataclass(frozen=True, slots=True)
+class DataTables:
+    models: set[str]
+    panels: list[dict[int, str]]
+
+
+def get_id(models: set[str], panels_list: list[dict[str, str]]) -> list[dict[int, str]]:
+    panels = []
+    id_models = {model: num for num, model in enumerate(models, 1)}
+    for panel in panels_list:
+        for key, value in panel.items():
+            panels.append({id_models[key]: value})
+    return panels
+
+
+def write_from_csv(filepath: Path) -> DataTables:
     with open(filepath, "r", newline="") as csvfile:
         reader = csv.DictReader(csvfile, delimiter=";")
-        return list(reader)
+        models = set()
+        panels = []
+        for i in reader:
+            models.add(i["models"])
+            panels.append({i["models"]: i["panels"]})
+        return DataTables(
+            models=models, panels=get_id(models=models, panels_list=panels)
+        )
 
 
 @dataclass(frozen=True, slots=True)
 class Request:
     query: str
-    data: list[dict[str, str]] | None
+    data: set[str] | list[dict[int, str]] | None
 
 
-def connect_db(query: str, data: list[dict[str, str]] | None = None) -> None:
-    with sqlite3.connect(db_file) as connect:
+def build_insert(table_name: str, columns: str) -> str:
+    split_columns = ", ".join("?" * len(columns.split(",")))
+    return insert.format(table_name, columns, split_columns)
+
+
+def connect_db(
+    filepath: Path, query: str, data: set[str] | list[dict[int, str]] | None = None
+) -> None:
+    with sqlite3.connect(filepath) as connect:
         request = Request(query=query, data=data)
         execute_request(cursor=connect.cursor(), request=request)
 
@@ -25,15 +54,37 @@ def connect_db(query: str, data: list[dict[str, str]] | None = None) -> None:
 def execute_request(cursor: sqlite3.Cursor, request: Request) -> None:
     if request.data:
         for data in request.data:
-            cursor.execute(request.query, tuple(data.values()))
+            if isinstance(data, dict):
+                cursor.execute(request.query, tuple(*data.items()))
+            else:
+                cursor.execute(request.query, (data,))
     else:
         cursor.execute(request.query)
 
 
 if __name__ == "__main__":
-    create_baker = """ CREATE TABLE IF NOT EXISTS multi_baker(id INTEGER PRIMARY KEY, models TEXT NOT NULL, panels TEXT NOT NULL) """
-    insert_baker = """ INSERT INTO multi_baker (id, models, panels) VALUES(?, ?, ?) """
     csv_file = Path("files/multi_baker.csv")
-    db_file = Path("files/multi_baker.db")
-    connect_db(query=create_baker)
-    connect_db(query=insert_baker, data=read_csv(csv_file))
+    baker_table = Path("files/multi_baker.db")
+    panel_table = Path("files/panels.db")
+    data_tables = write_from_csv(csv_file)
+    create = """ CREATE TABLE IF NOT EXISTS {}(id INTEGER PRIMARY KEY, {}) """
+    insert = """ INSERT INTO {} ({}) VALUES({}) """
+
+    connect_db(
+        filepath=baker_table, query=create.format("multi_baker", "models TEXT NOT NULL")
+    )
+    connect_db(
+        filepath=baker_table,
+        query=build_insert(table_name="multi_baker", columns="models"),
+        data=data_tables.models,
+    )
+
+    connect_db(
+        filepath=panel_table,
+        query=create.format("panels", "model_id INT NOT NULL, panels TEXT NOT NULL"),
+    )
+    connect_db(
+        filepath=panel_table,
+        query=build_insert(table_name="panels", columns="model_id, panels"),
+        data=data_tables.panels,
+    )
