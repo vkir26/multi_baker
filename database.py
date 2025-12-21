4e8fc9f46a2a83ac461a571d77b709407896f751
vkir26 <vanya.kireew2012@yandex.ru>
2025-12-21 17:39:05 +0300

    Changing click commands and Correction remark

--- database.py
+++ database.py
@@ -6,46 +6,31 @@ from sqlite3 import Cursor
 filepath_db = Path("files/multi_baker.db")
 
 
-def build_insert(table_name: str, columns: str) -> str:
-    insert = """ INSERT INTO {} ({}) \
-                 VALUES ({}) """
-    split_columns = ", ".join("?" * len(columns.split(",")))
-    return insert.format(table_name, columns, split_columns)
-
-
-@dataclass(frozen=True, slots=True)
-class Request:
-    query: str
-    data: set[str] | list[dict[int, str]] | None
-
-
-def connect_db(
-    table_name: str, columns: str, data: set[str] | list[dict[int, str]] | None = None
-) -> None:
-    query = build_insert(table_name=table_name, columns=columns)
+def insert_data(data: dict[str, list[str]]) -> None:
     try:
         with sqlite3.connect(filepath_db) as connect:
-            request = Request(query=query, data=data)
-            execute_request(cursor=connect.cursor(), request=request)
+            cursor = connect.cursor()
+            for model, panels in data.items():
+                cursor.execute(
+                    """ INSERT INTO multi_baker (model)
+                                   VALUES (?) """,
+                    (model,),
+                )
+                model_id = cursor.lastrowid
+                for panel in panels:
+                    cursor.execute(
+                        """ INSERT INTO panel (model_id, panel)
+                                       VALUES (?, ?) """,
+                        (model_id, panel),
+                    )
     except sqlite3.Error as e:
         print(f"Ошибка: {e}")
 
 
-def execute_request(cursor: sqlite3.Cursor, request: Request) -> None:
-    if request.data:
-        for data in request.data:
-            if isinstance(data, dict):
-                cursor.execute(request.query, tuple(*data.items()))
-            else:
-                cursor.execute(request.query, (data,))
-
-
-def data_request(fields: str, table: str) -> Cursor:
-    select = """ SELECT {}
-                 FROM {} """
+def data_request(query: str) -> Cursor:
     with sqlite3.connect(filepath_db) as connect:
         cursor = connect.cursor()
-        return cursor.execute(select.format(fields, table))
+        return cursor.execute(query)
 
 
 @dataclass(frozen=True, slots=True)
@@ -55,7 +40,10 @@ class BakerView:
 
 
 def get_bakers() -> list[BakerView]:
-    request = data_request(fields="id, model", table="multi_baker")
+    request = data_request(
+        query=""" SELECT id, model
+                                     FROM multi_baker """
+    )
     return [BakerView(id=id_model, model=model) for id_model, model in request]
 
 
@@ -67,14 +55,12 @@ class BakerWithPanels:
 
 def get_baker(baker_id: int) -> BakerWithPanels:
     request = data_request(
-        fields="mb.id, model, panel",
-        table="multi_baker mb JOIN panel p ON p.model_id = mb.id",
+        query=f""" SELECT mb.id, model, panel FROM multi_baker mb JOIN panel p ON p.model_id = mb.id WHERE mb.id = {baker_id}"""
     )
     baker = None
     panels = []
     for model_id, model, panel in request:
-        if model_id == baker_id:
-            if baker is None:
-                baker = model
-            panels.append(panel)
+        if baker is None:
+            baker = model
+        panels.append(panel)
     return BakerWithPanels(model=baker, panels=panels)
--- main.py
+++ main.py
@@ -1,53 +1,60 @@
+import csv
 import sqlite3
 import click
 from pathlib import Path
-from enum import StrEnum
-from multi_baker import read_from_csv
-from database import (
-    connect_db,
-    get_bakers,
-    get_baker
-)
+from database import insert_data, get_bakers, get_baker
 
 
-class Command(StrEnum):
-    PARSE = "parse",
-    BY_ID = "by_id",
-    MODELS = "models"
+def read_from_csv(filepath: Path) -> dict[str, list[str]]:
+    with open(filepath, "r", newline="") as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=";")
+        models: dict[str, list[str]] = {}
+        for i in reader:
+            model = i["models"]
+            if model not in models:
+                models[model] = []
+            models[model].append(i["panels"])
+        return models
 
 
 def fill_database() -> None:
-    connect_db(table_name="multi_baker",
-               columns="model",
-               data=data_tables.models)
-    connect_db(table_name="panel",
-               columns="model_id, panel",
-               data=data_tables.panels)
-
-
-@click.command()
-@click.option(f'--{Command.PARSE}', is_flag=True, help='Заполнить базу данных из CSV.')
-@click.option(f'--{Command.BY_ID}', help='Поиск модели по ID.', type=int)
-@click.option(f'--{Command.MODELS}', is_flag=True, help='Все доступные модели.')
-def main(**kwargs) -> None:
-    """Сервис мультипекарей."""
-    for command, value in kwargs.items():
-        if value is not None and value != False:
-            match command:
-                case Command.PARSE:
-                    fill_database()
-                case Command.BY_ID:
-                    click.echo(get_baker(value))
-                case Command.MODELS:
-                    click.echo("Список доступных моделей:")
-                    for baker in get_bakers():
-                        click.echo(baker)
-
-
-if __name__ == '__main__':
-    csv_file = Path("files/multi_baker.csv")
-    data_tables = read_from_csv(csv_file)
     try:
-        main()
+        insert_data(data=data)
     except sqlite3.OperationalError as e:
         print(f"Ошибка: {e}")
+
+
+@click.group()
+def multi_baker() -> None:
+    """Сервис мультипекарей."""
+    pass
+
+
+@multi_baker.command()
+@click.option("--parse", is_flag=True, help="Заполнить базу данных из CSV.")
+def file_parse(parse: bool) -> None:
+    fill_database()
+
+
+@multi_baker.command()
+@click.option("--by_id", help="Поиск модели по ID.", type=int)
+def get_model(by_id: int) -> None:
+    click.echo(get_baker(by_id))
+
+
+@multi_baker.command()
+@click.option("--all_models", is_flag=True, help="Все доступные модели.")
+def get_models(all_models: bool) -> None:
+    click.echo("Список доступных моделей:")
+    for baker in get_bakers():
+        click.echo(baker)
+
+
+def main() -> None:
+    multi_baker()
+
+
+if __name__ == "__main__":
+    csv_file = Path("files/multi_baker.csv")
+    data = read_from_csv(csv_file)
+    main()
